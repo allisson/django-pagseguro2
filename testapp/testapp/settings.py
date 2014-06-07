e140a8a017c5d85f038fd75946f5533c88167e4b
Allisson Azevedo <allisson@gmail.com>
2014-06-07 14:22:14 -0300

    Ajustes para suportar as versões 1.3 e 1.4 do Django.

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -120,36 +120,4 @@ INSTALLED_APPS = (
     'django_nose',
 )
 
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
-
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+try:
+    from django.conf.urls import patterns, include, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
