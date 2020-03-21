401f759c5b7dbb09be1865952956625c212e064a
Thomas Cenni <33039107+thomascenni@users.noreply.github.com>
2020-03-21 18:24:40 -0300

    Update for Django 3.X
    python_2_unicode_compatible dropped in django.utils.encoding starting from version 3.X

--- pagseguro/models.py
+++ pagseguro/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 from pagseguro.settings import PAGSEGURO_LOG_IN_MODEL
 from pagseguro.signals import (
